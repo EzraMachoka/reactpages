7b36e718f05f06cce52eb6ff908a94f3fef0a8b6
Ezra Machoka <ezra.machoka@student.moringaschool.com>
2023-09-27 11:31:46 +0300

    another one

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-// import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  // Define a state variable to hold the input value
+  const [number, setNumber] = useState(0);
+
+  // Define a function to handle the doubling of the number
+  const doubleNumber = () => {
+    setNumber(number * 2);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Edit <code>My github actions first trial</code> and save to reload.
-        </p>
-        <p>This is a simple react app that demonsrates usebofngithub actions</p>
-        <h2>React 1738</h2>
-      </header>
+    <div className="container mx-auto mt-8 text-center">
+      <h1 className="text-3xl mb-4">Double the Number</h1>
+      <input
+        type="number"
+        value={number}
+        onChange={(e) => setNumber(Number(e.target.value))}
+        className="border p-2 mb-4"
+      />
+      <button onClick={doubleNumber} className="bg-blue-500 text-white px-4 py-2 rounded">
+        Double
+      </button>
+      <div className="mt-4">
+        <strong>Result:</strong> {number}
+      </div>
     </div>
   );
 }
