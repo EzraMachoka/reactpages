4def5b9d0c77cfc95b76a9c48494d83ee96eafa1
Ezra Machoka <ezra.machoka@student.moringaschool.com>
2023-09-27 11:12:14 +0300

    and another one

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-    
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
           Edit <code>My github actions first trial</code> and save to reload.
         </p>
         <p>This is a simple react app that demonsrates usebofngithub actions</p>
-        <p>I will keep my ting brief, immigrants are jarring blud</p>
+        <h2>React 1738</h2>
       </header>
     </div>
   );
