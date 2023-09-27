914d70c33e908ab68223e01ce93718b233855dfd
Ezra Machoka <ezra.machoka@student.moringaschool.com>
2023-09-27 09:51:14 +0300

    further changes

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>This is a simple react app that demonsrates usebofngithub actions</p>
         <a
           className="App-link"
           href="https://reactjs.org"
